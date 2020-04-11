1d75936de868bbf5d29a06b6426aa1e0e68c7ce6
kiranbhore <kiranbhore583@gmail.com>
2020-04-11 14:08:54 +0530

    asdfgh

---
+++ ShreeFabricators/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ShreeFabricators
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ShreeFabricators/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShreeFabricators.MvcApplication" Language="C#" %>
